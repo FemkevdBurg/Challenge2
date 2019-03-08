274743852eb80dffd0dc1bd8672f2bfc0cab6c73
Femke <17067553@student.hhs.nl>
2019-03-08 16:35:32 +0100

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,94 @@
-//Klok
+
+function clock() {
+	var time = new Date(),
+
+	hours = time.getHours(),
+
+	minutes = time.getMinutes(),
+
+	seconds = time.getSeconds();
+
+  //hours = 21;
+
+  if (hours>6 & hours<13) {
+    animateMorning();
+  }
+
+  if (hours>12 & hours<20 ){
+    animateDay();
+  }
+
+  if (hours>19 || hours>0 & hours<6){
+    animateNight();
+  }
+
+	document.querySelectorAll('.clock')[0].innerHTML = harold(hours) + ":" + harold(minutes) + ":" + harold(seconds);
+
+    function harold(standIn) {
+		if (standIn < 10) {
+			standIn = '0' + standIn
+		}
+		return standIn;
+	}
+
+}
+
+setInterval(clock, 1000);
+
+
+//Veranderingen per moment van de dag
+function animateMorning(){
+  document.body.style.backgroundImage = "url('images/sunriseBackground.jpg')";
+  document.getElementById('tekst').innerHTML = "Het is momenteel ochtend";
+  document.getElementById('info').style.backgroundColor = "#FFB53C";
+  document.getElementById('picture').src = "images/sunrise.png";
+}
+
+function animateDay(){
+  document.body.style.backgroundImage = "url('images/dayBackground.jpg')";
+  document.getElementById('tekst').innerHTML = "Het is momenteel overdag";
+  document.getElementById('info').style.backgroundColor = "#F1D9BB";
+  document.getElementById('picture').src = "images/sun.png";
+}
+
+function animateNight(){
+  document.body.style.backgroundImage = "url('images/nightBackground.jpg')";
+  document.getElementById('tekst').innerHTML = "Het is momenteel nacht";
+  document.getElementById('info').style.backgroundColor = "#046379";
+  document.getElementById('picture').src = "images/moon.png";
+}
+//Veranderingen per moment van de dag
+
+//Datum
+var today = new Date();
+document.getElementById('date').innerHTML = today.getDate() + "-" + today.getMonth() + 1 + "-" + today.getFullYear();var d = new Date();
+//Datum
+
+
+//Animaties met TweenMax
+
+/*TweenMax.to(".logospacex", 6, {
+  left:600,
+  backgroundColor:"red",
+  padding: 20,
+  borderColor:"white",
+  borderRadius:"50%"
+});
+
+
+TweenMax.to(".logospacex", 6, {x:600, rotation:360, scale:0.5});
+TweenMax.to(".logospacex", 2, {x:600, ease:Back.easeOut});
+TweenMax.to(".logospacex", 2, {x:600, ease:Elastic.easeOut});
+TweenMax.to(".logospacex", 2, {x:600, ease:Bounce.easeOut});
+
+TweenMax.from(".logospacex", 0.5, {opacity:0, scale:0, ease:Bounce.easeOut});*/
+
+//Animaties met TweenMax
+
+
+
+
+/*//Klok
 //bron: https://greensock.com/forums/topic/18013-clock-animation-not-playing-on-setinterval/?tab=comments#comment-82442
 $(document).ready(function() {
   var datetime = new Date(),
@@ -6,18 +96,18 @@ $(document).ready(function() {
     m = datetime.getMinutes(),
     s = datetime.getSeconds();
 
-  //h = 12;
+  //h = 7;
 
-  if (h>7 & h<13) {
-    animateMorningBackground();
+  if (h>6 & h<13) {
+    animateMorning();
   }
 
   if (h>12 & h<20 ){
-    animateDayBackground();
+    animateDay();
   }
 
-  if (h>20 || h>0 & h<7){
-    animateNightBackground();
+  if (h>19 || h>0 & h<6){
+    animateNight();
   }
 
   var myPointerH = $(".clockH");
@@ -74,9 +164,9 @@ $(document).ready(function() {
     showTime();
   }, 1000);
 });
-//Klok
+//Klok*/
 
-
+/*
 //Tijd in cijfers
 //Bron: https://greensock.com/forums/topic/17225-flip-clock-effect/?tab=comments#comment-77132
 
@@ -150,47 +240,4 @@ function tick(mc,i, toZero=false){
   TweenMax.fromTo(mc.childNodes[7], .5+.2*Math.random(), {rotationX:90},{rotationX:0, delay:.1})
   TweenMax.fromTo(mc.childNodes[5], .6, {alpha:1},{alpha:0, delay:.1})
 }
-//Tijd in cijfers
-
-
-//Achtergrond + tekst
-function animateMorningBackground(){
-  //alert("Het is momenteel ochtend");
-  document.body.style.backgroundImage = "url('images/sunriseBackground.jpg')";
-  document.getElementById('tekst').innerHTML = "Het is momenteel ochtend";
-}
-
-function animateDayBackground(){
-  //alert("Het is momenteel overdag");
-  document.body.style.backgroundImage = "url('images/dayBackground.jpg')";
-  document.getElementById('tekst').innerHTML = "Het is momenteel overdag";
-}
-
-function animateNightBackground(){
-  //alert("Het is momenteel nacht");
-  document.body.style.backgroundImage = "url('images/nightBackground.jpg')";
-  document.getElementById('tekst').innerHTML = "Het is momenteel nacht";
-}
-//Achtergrond + tekst
-
-//Datum
-var today = new Date();
-document.getElementById('date').innerHTML = today.getDate() + "-" + today.getMonth() + 1 + "-" + today.getFullYear();var d = new Date();
-//Datum
-
-
-//TweenMax.to(".logospacex", 6, {
-  //left:600,
-  //backgroundColor:"red",
-  //padding: 20,
-  //borderColor:"white",
-  //borderRadius:"50%"
-//});
-
-
-/*TweenMax.to(".logospacex", 6, {x:600, rotation:360, scale:0.5});*/
-/*TweenMax.to(".logospacex", 2, {x:600, ease:Back.easeOut});*/
-/*TweenMax.to(".logospacex", 2, {x:600, ease:Elastic.easeOut});*/
-/*TweenMax.to(".logospacex", 2, {x:600, ease:Bounce.easeOut});*/
-
-//TweenMax.from(".logospacex", 0.5, {opacity:0, scale:0, ease:Bounce.easeOut});
+//Tijd in cijfers*/
